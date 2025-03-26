1fd227d6e70dda4011ca30b2b22018b802d6fe3a
Alejandro Juarez <ja24001@esfe.agape.edu.sv>
2025-03-26 16:01:22 -0600

    Quite unos using innecesarios de varias clases, revisamos el controlador de Login e hice Department, pendiente agregarle css a esos 2 y hacer los demas controllers q faltan

--- ExploreSV.BusinessLogic/UseCases/Users/Commads/DeleteUser/DeleteUserHandler.cs
+++ ExploreSV.BusinessLogic/UseCases/Users/Commads/DeleteUser/DeleteUserHandler.cs
@@ -1,12 +1,6 @@
-﻿using ExploreSV.BusinessLogic.UseCases.Roles.Commads.DeleteRole;
-using ExploreSV.DataAccess.Interfaces;
+﻿using ExploreSV.DataAccess.Interfaces;
 using ExploreSV.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExploreSV.BusinessLogic.UseCases.Users.Commads.DeleteUser;
 
--- ExploreSV.BusinessLogic/UseCases/Users/Queries/GetUser/GetUserHandler.cs
+++ ExploreSV.BusinessLogic/UseCases/Users/Queries/GetUser/GetUserHandler.cs
@@ -3,11 +3,6 @@ using ExploreSV.DataAccess.Interfaces;
 using ExploreSV.Entities;
 using Mapster;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExploreSV.BusinessLogic.UseCases.Users.Queries.GetUser;
 
